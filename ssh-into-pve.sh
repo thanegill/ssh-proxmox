bb5a27d25ca3c60ce45022784163eb67f2e0040c
Thane Gill <me@thanegill.com>
2018-05-04 12:30:38 -0700

    Fast, more generic, and exact name matching

--- ssh-into-pve.sh
+++ ssh-into-pve.sh
@@ -1,38 +1,42 @@
 #!/usr/bin/env bash
 set -e
 
-PVE_DOMAIN="${PVE_DOMAIN:localhost}"
+PVE_DOMAIN="${PVE_DOMAIN:-vanguard.vashonsd.org}"
+
+PVE_HOSTS=(pve1 pve2 pve3)
 
 function list_of_containers() {
-    for i in $(seq 1 3); do
-        ssh pve$i.$PVE_DOMAIN "pct list | tail -n +2" &
+    for i in "${PVE_HOSTS[@]}"; do
+        ssh $i.$PVE_DOMAIN "pct list | tail -n +2" &
     done
 }
 
 function containers_host() {
-    join <(ssh pve1.$PVE_DOMAIN "ha-manager status | grep 'ct:' | awk -F'[ :(),]' '{print \$3 \" \" \$5}' | sort" &) \
+    join <(ssh ${PVE_HOSTS[0]}.$PVE_DOMAIN "ha-manager status | grep 'ct:' | awk -F'[ :(),]' '{print \$3 \" \" \$5}' | sort" &) \
         <(list_of_containers | awk '{print $1 " " $3}' | sort &)
 }
 
 case $1 in
 list)
-    echo "Only Running Containers, no KVM VM's"
+    echo "Only running containers in HA manager, no KVM VM's"
     containers_host
 ;;
 help)
 cat <<-EOF
 	[list | <vmid> | <vm-name>]
-	Only works on running containers, no KVM VM's"
+	Only works on running containers in HA manager, no KVM VM's"
 	set PVE_DOMAIN to change the domain
 EOF
 ;;
 ''|*[0-9]*)
-    echo "Entering $(containers_host | grep $1 | awk '{ print $3 " (" $1 ") on " $2 }')"
-    ssh -tt $(containers_host | grep $1 | awk '{ print $2 }').$PVE_DOMAIN "pct enter $1"
+    container_host=$(containers_host | grep -Ew "$1$")
+    echo "Entering $(echo $container_host | awk '{ print $3 " (" $1 ") on " $2 }')"
+    ssh -tt $(echo $container_host | awk '{ print $2 }').$PVE_DOMAIN "pct enter $1"
 ;;
 *)
-    echo "Entering $(containers_host | grep $1 | awk '{ print $3 " (" $1 ") on " $2 }')"
-    ssh -tt $(containers_host | grep $1 | awk '{ print $2 }').$PVE_DOMAIN "pct enter $(containers_host | grep $1 | awk '{ print $1 }')"
+    container_host=$(containers_host | grep -Ew "$1$")
+    echo "Entering $(echo $container_host | awk '{ print $3 " (" $1 ") on " $2 }')"
+    ssh -tt $(echo $container_host | awk '{ print $2 }').$PVE_DOMAIN "pct enter $(echo $container_host | awk '{ print $1 }')"
 ;;
 esac
 
